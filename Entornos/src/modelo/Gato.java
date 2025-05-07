d514680c62d03999caae2ffca5753400e52f99f8
Usuario <Usuario@DESKTOP-127NUKK>
2025-05-07 18:17:02 +0200

    Añadimos la clase Gato

---
+++ Entornos/src/modelo/Gato.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Gato extends Animal{
+
+	public Gato(String nombre) {
+		super(nombre);
+		// TODO Auto-generated constructor stub
+	}
+	public void sonido() {
+		System.out.println("Miau");
+	}
+}
