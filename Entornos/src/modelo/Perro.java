cda01693d58774aa5f60826424dfd02959d9aaa7
Usuario <Usuario@DESKTOP-127NUKK>
2025-05-07 18:17:48 +0200

    Y por ultimo añadimos la clase Perro

---
+++ Entornos/src/modelo/Perro.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class Perro extends Animal{
+
+	public Perro(String nombre) {
+		super(nombre);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void sonido() {
+		System.out.println("Guau");
+	}
+}
